f1ca8d010a1cfcb912f41d163634867af012a1d6
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-05 20:59:50 -0300

    Vistas de Promociones

---
+++ src/services/PromocionService.ts
@@ -0,0 +1,4 @@
+import { IPromocion } from "../types/IPromociones";
+import { BackendClient } from "./BackendClient";
+
+export class PromocionService extends BackendClient<IPromocion> {}
---
+++ src/types/IPromociones.ts
@@ -0,0 +1,9 @@
+export interface IPromocion{
+    id: number;
+    image: string;
+    name: string;
+    since: string;
+    until: string;
+    description: string;
+    price: number;
+}
