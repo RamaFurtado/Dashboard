36cf81fa0bf4cbb42b0107ff776b4aaa1c6fcb72
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-08 19:56:01 -0300

    Avance switch alta y baja

--- src/components/ui/ButtonsTable/Switch.tsx
+++ src/components/ui/ButtonsTable/Switch.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ProductoService } from '../../../services/ProductoService';
 import Switch from '@mui/material/Switch';
 const API_URL = import.meta.env.VITE_API_URL;
@@ -12,20 +12,26 @@ interface ISwitchButton {
 
 export const SwitchButton = ({ id, currentState }: ISwitchButton) => {
     const [active, setActive] = React.useState<boolean>(true);
+    const [message, setMessage] = React.useState<string>(currentState ? 'Activo' : 'Inactivo');
 
-    const handleClick = () => {
+    const handleClick = async () => {
         console.log(active)
         console.log(id)
         if (active) {
             productoService.logicDelete(id);
+            await setMessage('Inactivo');
         } else {
             productoService.logicRestore(id);
+            await setMessage('Activo');
         }
         setActive(!active);
     }
+
     return (
         <div>
-            {currentState ? <Switch onClick={() => { productoService.logicDelete(id); console.log(id) }} defaultChecked /> : <Switch onClick={() => { productoService.logicRestore(id) }} />}
+            {message}
+
+            {currentState ? <Switch onClick={() => { handleClick() }} defaultChecked /> : <Switch onClick={() => { handleClick() }} />}
         </div>
     )
 }
