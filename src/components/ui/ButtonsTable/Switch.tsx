460eb496228a28534b000369d13fee738f8d7056
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-09 09:03:03 -0300

    Configuración del Switch

--- src/components/ui/ButtonsTable/Switch.tsx
+++ src/components/ui/ButtonsTable/Switch.tsx
@@ -1,37 +1,52 @@
-import React, { useEffect } from 'react'
-import { ProductoService } from '../../../services/ProductoService';
-import Switch from '@mui/material/Switch';
+import React, { useEffect } from "react";
+import { ProductoService } from "../../../services/ProductoService";
+import Switch from "@mui/material/Switch";
+import { CircularProgress } from "@mui/material";
 const API_URL = import.meta.env.VITE_API_URL;
 
-const productoService = new ProductoService(API_URL + '/products');
+const productoService = new ProductoService(API_URL + "/products");
 
 interface ISwitchButton {
-    id: number;
-    currentState: boolean;
+  id: number;
+  currentState: boolean;
 }
 
 export const SwitchButton = ({ id, currentState }: ISwitchButton) => {
-    const [active, setActive] = React.useState<boolean>(true);
-    const [message, setMessage] = React.useState<string>(currentState ? 'Activo' : 'Inactivo');
+  const [active, setActive] = React.useState<boolean>(currentState);
+  const [message, setMessage] = React.useState<string>(
+    currentState ? "Activo" : "Inactivo"
+  );
+  const [loader, setLoader] = React.useState<boolean>(false);
 
-    const handleClick = async () => {
-        console.log(active)
-        console.log(id)
-        if (active) {
-            productoService.logicDelete(id);
-            await setMessage('Inactivo');
-        } else {
-            productoService.logicRestore(id);
-            await setMessage('Activo');
-        }
-        setActive(!active);
+  const handleClick = async () => {
+    setLoader(true); // Activar el loader antes de las operaciones asincrónicas
+    try {
+      if (active) {
+        await productoService.logicDelete(id);
+        setMessage("Inactivo");
+        console.log("Dada de baja correcta");
+      } else {
+        await productoService.logicRestore(id);
+        setMessage("Activo");
+        console.log("Dada de alta correcta");
+      }
+      setActive(!active);
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoader(false); // Desactivar el loader después de las operaciones asincrónicas
     }
-
-    return (
-        <div>
-            {message}
-
-            {currentState ? <Switch onClick={() => { handleClick() }} defaultChecked /> : <Switch onClick={() => { handleClick() }} />}
-        </div>
-    )
-}
+  };
+  return (
+    <div>
+      {message}
+      {loader ? <CircularProgress/> : (
+                <Switch
+                    checked={active} // Utiliza el estado local 'active' para controlar el estado del Switch
+                    onChange={() => handleClick()}
+                />
+            )}
+      {/* {currentState ? <Switch onClick={() => { handleClick() }} defaultChecked /> : <Switch onClick={() => { handleClick() }} />} */}
+    </div>
+  );
+};
