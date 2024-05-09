088b7c059b07482053c76cf59c76ed2d37b83b59
MIsabelJ <maria.jurman@alumnos.frm.utn.edu.ar>
2024-05-09 11:31:21 -0300

    Arreglo servidor para que muestre todo el db.json

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 import jsonServer from 'json-server';
+import path from 'path';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.get('/', (req, res) => {
+  res.sendFile(path.resolve('db.json'));
+});
+
 server.use(middlewares);
 server.use(router);
 
-// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
   console.log(`JSON Server está funcionando en el puerto ${PORT}`);
 });
