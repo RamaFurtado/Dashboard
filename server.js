608da1896e158287b4f84f504265838cf63daca6
GabrielGiorgis <gabi.giorgis6@gmail.com>
2024-05-15 17:02:36 -0300

    Servidor modificado

--- server.js
+++ server.js
@@ -3,24 +3,12 @@ import path from "path";
 import cors from "cors";
 
 const server = jsonServer.create();
+
+// Configuración básica de CORS para permitir solicitudes desde cualquier origen
 server.use(cors());
 
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
-const domainList = ["http://localhost:5173/"];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (domainList.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-
-server.use(cors(corsOptions));
 
 server.get("/", (req, res) => {
   res.sendFile(path.resolve("db.json"));
@@ -28,11 +16,6 @@ server.get("/", (req, res) => {
 
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "*");
-  next();
-});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
