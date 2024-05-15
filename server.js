7cc3cacb199b022c1670d565f1a3f9235016c120
AgusMandolesi <agustin.mandolesi@alumnos.frm.utn.edu.ar>
2024-05-15 15:45:34 -0300

    cambios para el cors

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import jsonServer from 'json-server';
-import path from 'path';
+import jsonServer from "json-server";
+import path from "path";
 // cors policy
-import cors from 'cors';
+import cors from "cors";
 
 const corsOptions = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-}
-server.use(cors(corsOptions));
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+};
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-server.get('/', (req, res) => {
-    res.sendFile(path.resolve('db.json'));
+server.get("/", (req, res) => {
+  res.sendFile(path.resolve("db.json"));
 });
 
 server.use(middlewares);
 server.use(router);
+server.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`JSON Server está funcionando en el puerto ${PORT}`);
+  console.log(`JSON Server está funcionando en el puerto ${PORT}`);
 });
